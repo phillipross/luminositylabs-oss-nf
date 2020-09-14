aca2438d49c1872e6b7c32a4a993d6881ef3cf46
Phillip Ross <phillip.w.g.ross@gmail.com>
2020-09-14 19:52:15 -0400

    travis-ci reconfig

--- .travis-ci/install-zulu11.sh
+++ .travis-ci/install-zulu11.sh
@@ -5,7 +5,6 @@ set -euf
 AZUL_GPG_KEY=0xB1998361219BD9C9
 ZULU_VERSION=11
 ZULU_RELEASE=11.0.8-1
-#JAVA_HOME=/usr/lib/jvm/zulu${ZULU_VERSION}
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys ${AZUL_GPG_KEY}
 sudo apt-add-repository 'deb http://repos.azulsystems.com/ubuntu stable main'
@@ -17,7 +16,7 @@ sudo sed -i.orig -e "s/^hl /jre /g" -e "s/^jdkhl /jdk /g" /usr/lib/jvm/.zulu${ZU
 sudo update-java-alternatives --set zulu${ZULU_VERSION}-ca-amd64
 export ALTERNATIVES_JAVAC=$(realpath /etc/alternatives/javac)
 export JAVA_HOME=${ALTERNATIVES_JAVAC%/bin/javac}
-#export PATH=$JAVA_HOME/bin:$PATH
+export PATH=$JAVA_HOME/bin:$PATH
 java -version
 
 set +euf
--- .travis-ci/install-zulu8.sh
+++ .travis-ci/install-zulu8.sh
@@ -5,7 +5,6 @@ set -euf
 AZUL_GPG_KEY=0xB1998361219BD9C9
 ZULU_VERSION=8
 ZULU_RELEASE=8.0.265-5
-#JAVA_HOME=/usr/lib/jvm/zulu-8-amd64
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys ${AZUL_GPG_KEY}
 sudo apt-add-repository 'deb http://repos.azulsystems.com/ubuntu stable main'
@@ -17,7 +16,7 @@ sudo sed -i.orig -e "s/^hl /jre /g" -e "s/^jdkhl /jdk /g" /usr/lib/jvm/.zulu${ZU
 sudo update-java-alternatives --set zulu${ZULU_VERSION}-ca-amd64
 export ALTERNATIVES_JAVAC=$(realpath /etc/alternatives/javac)
 export JAVA_HOME=${ALTERNATIVES_JAVAC%/bin/javac}
-#export PATH=$JAVA_HOME/bin:$PATH
+export PATH=$JAVA_HOME/bin:$PATH
 java -version
 
 set +euf
